801115eda5b808940f402425f490cf1d31946ffd
Ygor P <ygor.the.moster@gmail.com>
2020-08-04 21:11:11 -0300

    microinteractions beggining

---
+++ js/post.js
@@ -0,0 +1,1 @@
+var color;u("a[href='#back']").handle("click",function(){return history.back()}),color=u(".border").nodes[0].style["background-color"],u(document).on("scroll",function(){var progress=window.scrollY/window.scrollMaxY*100;return u("#progress").nodes[0].style.background="linear-gradient(to right, "+color+" "+progress+"%, white "+progress+"%)"});
